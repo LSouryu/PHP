f4922607fd59dba9d2ab6a6a05ea845136efc30d
Nathan Allary <n.allary@hotmail.com>
2023-07-05 17:38:20 +0200

    Assurance4

--- Assurance/Assurance_v2.php
+++ Assurance/Assurance_v2.php
@@ -10,11 +10,11 @@ if($agepermie < 2) {$b == false;} else {$b == true;}
 if($client < 5) {$c == false;} else {$c == true;}
 if ($a == true && $b == true && $c ==true && $accident == 0){
     echo "tarif bleu";
-} else if (($a == true && $b == true && $c && $accident == 0) || ($a == true && $b == true && $c == true && $accident == 1) || ($a && $b == true && $c == true && $accident == 0) || ($a == true && $b && $c == true && $accident == 0)){
+} else if (($a == true && $b == true && $c == false && $accident == 0) || ($a == true && $b == true && $c == true && $accident == 1) || ($a == false && $b == true && $c == true && $accident == 0) || ($a == true && $b == false && $c == true && $accident == 0)){
     echo "tarif vert";
-} else if (($a == true && $b == true && $c && $accident == 1) || ($a == true && $b == true && $c == true && $accident == 2) || ($a && $b == true && $c && $accident == 0) || ($a == true && $b && $c && $accident == 0) || ($a && $b == true && $c == true && $accident == 1) || ($a == true && $b && $c == true && $accident == 1) || ($a && $b && $c == true && $accident == 0)){
+} else if (($a == true && $b == true && $c == false && $accident == 1) || ($a == true && $b == true && $c == true && $accident == 2) || ($a == false && $b == true && $c == false && $accident == 0) || ($a == true && $b == false && $c == false && $accident == 0) || ($a == false && $b == true && $c == true && $accident == 1) || ($a == true && $b == false && $c == true && $accident == 1) || ($a == false && $b == false && $c == true && $accident == 0)){
     echo "tarif orange";
-}else if (($a && $b && $c && $accident == 0) || ($a == true && $b && $c && $accident == 1) || ($a && $b == true && $c && $accident == 1) || ($a == true && $b == true && $c && $accident == 2)){
+}else if (($a == false && $b == false && $c == false && $accident == 0) || ($a == true && $b == false && $c == false && $accident == 1) || ($a == false && $b == true && $c == false && $accident == 1) || ($a == true && $b == true && $c == false && $accident == 2)){
     echo "tarif rouge";
 } else {
     echo "pas assurée";
