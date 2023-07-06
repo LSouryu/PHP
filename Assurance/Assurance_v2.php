a52c2718b23825f2472b672d850c5185e1e5636a
LSouryu <n.allary@hotmail.com>
2023-07-06 08:48:08 +0200

    Assurance8

--- Assurance/Assurance_v2.php
+++ Assurance/Assurance_v2.php
@@ -1,14 +1,10 @@
 <?php
-$a = false; $b = false; $c = false;
-$age = readline("indiquer votre age : ") . "\n";
-$agepermie = readline("depuis combient de temps avez-vous votre permie ? ") . "\n";
-$accident = readline("nombre d'accident (si aucun : 0) : ") . "\n";
-$client = readline("client depuis combien de temps ? ") . "\n";
-if($age < 25) {$a = false;} else {$a = true;}
-if($agepermie < 2) {$b = false;} else {$b = true;}
-if($client < 5) {$c = false;} else {$c = true;}
-if ($a == true && $b == true && $c ==true && $accident == 0){ echo "tarif bleu"; } 
-else if (($a == true && $b == true && $c == false && $accident == 0) || ($a == true && $b == true && $c == true && $accident == 1) || ($a == false && $b == true && $c == true && $accident == 0) || ($a == true && $b == false && $c == true && $accident == 0)){ echo "tarif vert"; } 
-else if (($a == true && $b == true && $c == false && $accident == 1) || ($a == true && $b == true && $c == true && $accident == 2) || ($a == false && $b == true && $c == false && $accident == 0) || ($a == true && $b == false && $c == false && $accident == 0) || ($a == false && $b == true && $c == true && $accident == 1) || ($a == true && $b == false && $c == true && $accident == 1) || ($a == false && $b == false && $c == true && $accident == 0)){ echo "tarif orange"; }
-else if (($a == false && $b == false && $c == false && $accident == 0) || ($a == true && $b == false && $c == false && $accident == 1) || ($a == false && $b == true && $c == false && $accident == 1) || ($a == true && $b == true && $c == false && $accident == 2)){ echo "tarif rouge"; } 
+$a = 0; $b = 0; $c = 0; $age = readline("indiquer votre age : ") . "\n"; $agepermie = readline("depuis combient de temps avez-vous votre permie ? ") . "\n"; $accident = readline("nombre d'accident (si aucun : 0) : ") . "\n"; $client = readline("client depuis combien de temps ? ") . "\n";
+if($age < 25) {$a = 0;} else {$a = 1;}
+if($agepermie < 2) {$b = 0;} else {$b = 1;}
+if($client < 5) {$c = 0;} else {$c = 1;}
+if ($a == 1 && $b == 1 && $c ==1 && $accident == 0){ echo "tarif bleu"; } 
+else if (($a == 1 && $b == 1 && $c == 0 && $accident == 0) || ($a == 1 && $b == 1 && $c == 1 && $accident == 1) || ($a == 0 && $b == 1 && $c == 1 && $accident == 0) || ($a == 1 && $b == 0 && $c == 1 && $accident == 0)){ echo "tarif vert"; } 
+else if (($a == 1 && $b == 1 && $c == 0 && $accident == 1) || ($a == 1 && $b == 1 && $c == 1 && $accident == 2) || ($a == 0 && $b == 1 && $c == 0 && $accident == 0) || ($a == 1 && $b == 0 && $c == 0 && $accident == 0) || ($a == 0 && $b == 1 && $c == 1 && $accident == 1) || ($a == 1 && $b == 0 && $c == 1 && $accident == 1) || ($a == 0 && $b == 0 && $c == 1 && $accident == 0)){ echo "tarif orange"; }
+else if (($a == 0 && $b == 0 && $c == 0 && $accident == 0) || ($a == 1 && $b == 0 && $c == 0 && $accident == 1) || ($a == 0 && $b == 1 && $c == 0 && $accident == 1) || ($a == 1 && $b == 1 && $c == 0 && $accident == 2)){ echo "tarif rouge"; } 
 else { echo "pas assurée"; }
