a5ac9e2c1a932bae46f75068408ab1c8192786bf
LSouryu <n.allary@hotmail.com>
2023-07-06 08:31:52 +0200

    Assurance5

--- Assurance/Assurance_v2.php
+++ Assurance/Assurance_v2.php
@@ -5,9 +5,9 @@ $age = readline("indiquer votre age : ") . "\n";
 $agepermie = readline("depuis combient de temps avez-vous votre permie ? ") . "\n";
 $accident = readline("nombre d'accident (si aucun : 0) : ") . "\n";
 $client = readline("client depuis combien de temps ? ") . "\n";
-if($age < 25) {$a == false;} else {$a == true;}
-if($agepermie < 2) {$b == false;} else {$b == true;}
-if($client < 5) {$c == false;} else {$c == true;}
+if($age < 25) {$a = false;} else {$a = true;}
+if($agepermie < 2) {$b = false;} else {$b = true;}
+if($client < 5) {$c = false;} else {$c = true;}
 if ($a == true && $b == true && $c ==true && $accident == 0){
     echo "tarif bleu";
 } else if (($a == true && $b == true && $c == false && $accident == 0) || ($a == true && $b == true && $c == true && $accident == 1) || ($a == false && $b == true && $c == true && $accident == 0) || ($a == true && $b == false && $c == true && $accident == 0)){
